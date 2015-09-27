8fcc833babb2e13d64ccde7b5e33c703792ebf34
Filip Mitkovski <phichoo@gmail.com>
2015-09-28 00:41:29 +0200

    two new tests added for testing Apply action

--- BankingSite.ControllerTests/LoanApplicationControllerTests.cs
+++ BankingSite.ControllerTests/LoanApplicationControllerTests.cs
@@ -21,5 +21,40 @@ namespace BankingSite.ControllerTests
             sut.WithCallTo(x => x.Apply()).ShouldRenderDefaultView();
 
         }
+
+        [Test]
+        public void ShouldRedirectToAcceptedViewOnSuccessfulApplication()
+        {
+            var fakeRepo = new Mock<IRepository>();
+            var fakeAppScorer = new Mock<ILoanApplicationScorer>();
+
+            var sut = new LoanApplicationController(fakeRepo.Object, fakeAppScorer.Object);
+
+            var application = new LoanApplication()
+            {
+                IsAccepted = true,
+            };
+
+            sut.WithCallTo(x => x.Apply(application)).ShouldRedirectTo<int>(x => x.Accepted);
+
+        }
+
+        [Test]
+        public void ShouldRedirectToDeclineViewOnUnsuccessfulApplication()
+        {
+            var fakeRepo = new Mock<IRepository>();
+            var fakeAppScorer = new Mock<ILoanApplicationScorer>();
+
+            var sut = new LoanApplicationController(fakeRepo.Object, fakeAppScorer.Object);
+
+            var application = new LoanApplication()
+            {
+                IsAccepted = false,
+            };
+
+            sut.WithCallTo(x => x.Apply(application)).ShouldRedirectTo<int>(x => x.Declined);
+
+        }
     }
+
 }
