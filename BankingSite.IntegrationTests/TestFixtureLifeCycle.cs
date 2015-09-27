a1e1d02c40a97f76fd12c07befecbc44b507b9cd
Filip Mitkovski <phichoo@gmail.com>
2015-09-27 23:08:10 +0200

    git init

---
+++ BankingSite.IntegrationTests/TestFixtureLifeCycle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+
+namespace BankingSite.IntegrationTests
+{
+    public class TestFixtureLifeCycle
+    {
+        public TestFixtureLifeCycle()
+        {
+            EnsureDataDirectoryConnectionStringPlaceHolderIsSet();
+
+            EnsoureNoExistingDatabaseFiles();
+
+        }
+
+        private void EnsoureNoExistingDatabaseFiles()
+        {
+            const string connString = "name=DefaultConnection";
+
+            if (Database.Exists(connString))
+            {
+                Database.Delete(connString);
+            }
+        }
+
+        private static void EnsureDataDirectoryConnectionStringPlaceHolderIsSet()
+        {
+           AppDomain.CurrentDomain.SetData("DataDirectory", NUnit.Framework.TestContext.CurrentContext.TestDirectory);
+        }
+    }
+}
---
+++ BankingSite.UnitTests/LoanApplicationScorerTests.cs
@@ -0,0 +1,44 @@
+﻿using BankingSite.Models;
+using Moq;
+using NUnit.Framework;
+
+namespace BankingSite.UnitTests
+{
+    [TestFixture]
+    public class LoanApplicationScorerTests
+    {
+        public void ShouldDeclineWhenNotTooYoungAndWWealthyButPoorCredit_Classic()
+        {
+            var sut = new LoanApplicationScorer(new CreditHistoryChecker());
+
+            var application = new LoanApplication()
+            {
+                FirstName = "Sarah",
+                LastName = "Smith",
+                Age = 22,
+                AnnualIncome = 1000000.01m
+            };
+
+            sut.ScoreApplication(application);
+
+            Assert.That(application.IsAccepted, Is.False);
+        }
+
+        public void ShouldDeclineWhenNotTooYoungAndWWealthyButPoorCredit()
+        {
+            var fakeCreditCartChacker = new Mock<ICreditHistoryChecker>();
+            fakeCreditCartChacker.Setup(x => x.CheckCreditHistory(It.IsAny<string>(), It.IsAny<string>())).Returns(true);
+            var sut = new LoanApplicationScorer(fakeCreditCartChacker.Object);
+
+            var application = new LoanApplication()
+            {
+                Age = 22,
+                AnnualIncome = 1000000.01m
+            };
+
+            sut.ScoreApplication(application);
+
+            Assert.That(application.IsAccepted, Is.False);
+        }
+    }
+}
