8510199cd71eb38529175c546521d7d637e61314
Filip Mitkovski <phichoo@gmail.com>
2015-09-28 01:09:41 +0200

    LoanApplicationSearchController testing class is added to project with 2 tests

---
+++ BankingSite.ControllerTests/LoanApplicationSearchControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using BankingSite.Controllers;
+using BankingSite.Models;
+using Moq;
+using NUnit.Framework;
+using TestStack.FluentMVCTesting;
+
+namespace BankingSite.ControllerTests
+{
+    [TestFixture]
+    public class LoanApplicationSearchControllerTests
+    {
+        [Test]
+        public void ShouldReturn4045StatusWhenLoanIdNotFound()
+        {
+            var fakeRepo = new Mock<IRepository>();
+            var sut = new LoanApplicationSearchController(fakeRepo.Object);
+            sut.WithCallTo(x => x.ApplicationStatus(99)).ShouldGiveHttpStatus(HttpStatusCode.NotFound);
+        }
+
+        [Test]
+        public void ShouldRenderApplicationWhenIdExists()
+        {
+            const string fakeName = "Phill";
+            var fakeRepo = new Mock<IRepository>();
+            fakeRepo.Setup(f => f.Find(99)).Returns(new LoanApplication() {FirstName = fakeName }); 
+            var sut = new LoanApplicationSearchController(fakeRepo.Object);
+            sut.WithCallTo(x => x.ApplicationStatus(99))
+                .ShouldRenderDefaultView()
+                .WithModel<LoanApplication>(la => la.FirstName == fakeName);
+        }
+    }
+}
