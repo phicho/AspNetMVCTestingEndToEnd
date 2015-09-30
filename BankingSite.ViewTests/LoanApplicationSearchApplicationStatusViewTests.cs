250c677ea657faaf32969a4b102ad2f13749bf86
Filip Mitkovski <phichoo@gmail.com>
2015-09-30 16:44:56 +0200

    added test class for loan application search controller

---
+++ BankingSite.ViewTests/LoanApplicationSearchApplicationStatusViewTests.cs
@@ -0,0 +1,27 @@
+﻿using BankingSite.Models;
+using BankingSite.Views.LoanApplicationSearch;
+using HtmlAgilityPack;
+using NUnit.Framework;
+using RazorGenerator.Testing;
+
+namespace BankingSite.ViewTests
+{
+    [TestFixture]
+    public class LoanApplicationSearchApplicationStatusViewTests
+    {
+        [Test]
+        public void ShouldRenderAcceptedMessage()
+        {
+            var sut = new ApplicationStatus();
+            var model = new LoanApplication()
+            {
+                IsAccepted = true,
+            };
+            HtmlDocument html = sut.RenderAsHtml(model);
+            var isAcceptedMsgRenderd = html.GetElementbyId("acceptedMessage") != null;
+            var isDeclinedMsgRenderd = html.GetElementbyId("declinedMessage") != null;
+            Assert.That(isAcceptedMsgRenderd, Is.True);
+            Assert.That(isDeclinedMsgRenderd, Is.False);
+        }
+    }
+}
